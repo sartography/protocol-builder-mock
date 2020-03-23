6721895876f899bc036f35ae6de1607804331ca4
Dan Funk <daniel.h.funk@gmail.com>
2020-03-23 16:21:39 -0400

    Minor fix, trying to get travis to run successfully.

--- config/travis-testing.py
+++ config/travis-testing.py
@@ -5,5 +5,5 @@ NAME = "CR Connect Protocol Builder Mock"
 CORS_ENABLED = False
 DEVELOPMENT = True
 TESTING = True
-SQLALCHEMY_DATABASE_URI = "postgresql://crc_user:crc_pass@localhost:5432/pb_test"
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost:5432/pb_test"
 SECRET_KEY = 'a really really really really long secret key'
