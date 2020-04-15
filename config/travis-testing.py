d68079b247435320501160b43b69013bf499897d
Aaron Louie <ajlouie@gmail.com>
2020-04-15 14:02:52 -0400

    Prints some config settings for debugging

--- config/travis-testing.py
+++ config/travis-testing.py
@@ -7,3 +7,8 @@ DEVELOPMENT = True
 TESTING = True
 SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost:5432/pb_test"
 SECRET_KEY = 'a really really really really long secret key'
+
+print('+++ USING TRAVIS TESTING CONFIG: +++')
+print('SQLALCHEMY_DATABASE_URI = ', SQLALCHEMY_DATABASE_URI)
+print('DEVELOPMENT = ', DEVELOPMENT)
+print('TESTING = ', TESTING)
