07444f8c3652a74600335c92627dfb4f8065f6bb
Aaron Louie <ajlouie@gmail.com>
2020-06-30 14:58:29 -0400

    Moves delete column to right side

--- pb/forms.py
+++ pb/forms.py
@@ -55,11 +55,6 @@ class StudyTable(Table):
         anchor_attrs={'class': 'btn btn-icon btn-primary', 'title': 'Edit Study'},
         th_html_attrs={'class': 'mat-icon text-center', 'title': 'Edit Study'}
     )
-    delete = LinkCol(
-        'delete', 'del_study', url_kwargs=dict(study_id='STUDYID'),
-        anchor_attrs={'class': 'btn btn-icon btn-warn', 'title': 'Delete Study'},
-        th_html_attrs={'class': 'mat-icon text-center', 'title': 'Delete Study'}
-    )
     details = LinkCol(
         'ballot', 'study_details', url_kwargs=dict(study_id='STUDYID'),
         anchor_attrs={'class': 'btn btn-icon btn-default', 'title': 'Edit Questions'},
@@ -77,4 +72,9 @@ class StudyTable(Table):
     Q_COMPLETE = BoolCol('Complete?')
     requirements = NestedTableCol('Requirements', RequirementsTable)
     investigators = NestedTableCol('Investigators', InvestigatorsTable)
+    delete = LinkCol(
+        'delete', 'del_study', url_kwargs=dict(study_id='STUDYID'),
+        anchor_attrs={'class': 'btn btn-icon btn-warn', 'title': 'Delete Study'},
+        th_html_attrs={'class': 'mat-icon text-center', 'title': 'Delete Study'}
+    )
 
