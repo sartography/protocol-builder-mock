5b8c3284d9c50429ad86cdc50274a5518fb2cb0d
mike cullerton <michaelc@cullerton.com>
2021-06-23 12:31:10 -0400

    Changed the test database to use Postgres, instead of SQLite.

--- config/testing.py
+++ config/testing.py
@@ -8,7 +8,16 @@ NAME = "CR Connect Protocol Builder Mock"
 CORS_ALLOW_ORIGINS = re.split(r',\s*', environ.get('CORS_ALLOW_ORIGINS', default="localhost:5000"))
 DEVELOPMENT = True
 TESTING = True
-SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI', default="sqlite:///app.db")
+DB_HOST = environ.get('DB_HOST', default="localhost")
+DB_PORT = environ.get('DB_PORT', default="5432")
+DB_NAME = environ.get('DB_NAME', default="pb_test")
+DB_USER = environ.get('DB_USER', default="crc_user")
+DB_PASSWORD = environ.get('DB_PASSWORD', default="crc_pass")
+SQLALCHEMY_DATABASE_URI = environ.get(
+    'SQLALCHEMY_DATABASE_URI',
+    default="postgresql://%s:%s@%s:%s/%s" % (DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+)
+
 SECRET_KEY = 'a really really really really long secret key'
 WTF_CSRF_ENABLED = False
 
